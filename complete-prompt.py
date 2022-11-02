2f717fa4ba0294a130f3a73b55e7b14d04e563a2
strikingLoo <lucianostrika44@gmail.com>
2022-11-02 15:41:57 -0300

    first commit. Added 3 modes of using and should be easy enough to extend with more preambles or formats

---
+++ complete-prompt.py
@@ -0,0 +1,48 @@
+import os
+import openai
+import sys
+
+SMART_QUESTION_PREAMBLE = 'The following is a dialogue where a very intelligent artificial intelligence answers questions truthfully and to the best of its abilities. Q: %s A:'
+SMART_EXPLANATION_PREAMBLE = 'The following is a short essay by a very intelligent artificial intelligence who explains and summarizes, truthfully and to the best of his abilities, the following topic. Topic: %s'
+TUTORIAL_PREAMBLE = 'The following is a tutorial where a very intelligent person explains how to solve a problem. Problem: %s Solution: Let\'s think step by step.'
+
+def format_prompt(mode, prompt):
+  if mode == 1:
+    return SMART_QUESTION_PREAMBLE % prompt
+  elif mode == 2:
+    return SMART_EXPLANATION_PREAMBLE % prompt
+  elif mode == 3:
+    return TUTORIAL_PREAMBLE % prompt
+  else:
+    return prompt
+
+def request_completion(final_prompt, model_name = "text-davinci-002", temperature=.7):
+  openai.api_key = os.getenv("OPENAI_API_KEY")
+
+  response = openai.Completion.create(
+    model=model_name,
+    prompt=final_prompt,
+    temperature=temperature,
+    max_tokens=512,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+  )
+  return response
+
+def answer_prompt(mode, request_prompt):
+  final_prompt = format_prompt(mode, request_prompt)
+  print(final_prompt)
+  response = request_completion(final_prompt)
+  answer_text = response['choices'][0]['text']
+  return answer_text
+
+try:
+  mode = int(sys.argv[1])
+except:
+  mode = -1
+
+request_prompt = sys.argv[2]
+
+final_answer = answer_prompt(mode, request_prompt)
+print(final_answer)
